b8f2cdc69cb7f308e7709009bec4972808fdbe75
Ccdjh.Marx.Mac <Ccdjh.Marx@gmail.com>
2016-06-26 15:38:07 +0800

    add setup.py
    0.0.1

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'boxwilddog',
+    version = '0.0.1',
+    keywords = ('wilddog', 'json', 'db', 'value'),
+    description = 'wilddog tool',
+    license = 'MIT License',
+    author = 'CcdjhMarx',
+    author_email = 'ccdjh.marx@all.com',
+    packages = ['boxmongodb'],
+)
